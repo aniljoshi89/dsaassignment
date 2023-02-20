d3b25798cd528d9e04b2708b3cb0fd60658350c8
aniljoshi89 <125785293+aniljoshi89@users.noreply.github.com>
2023-02-20 18:42:52 +0530

    Update Q12.cpp

--- DSA Assignment/Q12.cpp
+++ DSA Assignment/Q12.cpp
@@ -1,25 +1,19 @@
 #include<iostream>
 #include<string>
 using namespace std;
-//function to check if the string is Palindrome
 int isPalindrome(char *str, int n)
 {
- //the first index
  int start_index = 0;
- 
- //the last index
  int last_index = n-1;
  
  while(start_index < last_index && str[start_index] == str[last_index])
  {
-  //increment start index and decrement last index
   start_index++;
   last_index--;
  }
  
  if(start_index < last_index)
  {
-  //this means that we did not reach the center
   cout<<"NOT A PALINDROME"<<endl;
   return 0;
  }
