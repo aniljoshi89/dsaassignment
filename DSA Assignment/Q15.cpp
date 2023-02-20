c4352ce03569d3e47419963d895ed9820ed9c3b6
aniljoshi89 <125785293+aniljoshi89@users.noreply.github.com>
2023-02-20 18:48:57 +0530

    Update Q15.cpp

--- DSA Assignment/Q15.cpp
+++ DSA Assignment/Q15.cpp
@@ -4,23 +4,16 @@ using namespace std;
 //push -> o(n)
 //pop -> o(1)
 class Stack {
-	// Two inbuilt queues
 	queue<int> q1, q2;
 
 public:
 	void push(int x)
 	{
-		// Push x first in empty q2
 		q2.push(x);
-
-		// Push all the remaining
-		// elements in q1 to q2.
 		while (!q1.empty()) {
 			q2.push(q1.front());
 			q1.pop();
 		}
-
-		// swap the names of two queues
 		queue<int> q = q1;
 		q1 = q2;
 		q2 = q;
@@ -28,7 +21,6 @@ public:
 
 	void pop()
 	{
-		// if no elements are there in q1
 		if (q1.empty())
 			return;
 		q1.pop();
