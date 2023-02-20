1915f7861c3c20e95e04869221fcce2d21f3cf0c
aniljoshi89 <125785293+aniljoshi89@users.noreply.github.com>
2023-02-20 18:47:44 +0530

    Update Q14.cpp

--- DSA Assignment/Q14.cpp
+++ DSA Assignment/Q14.cpp
@@ -11,31 +11,21 @@ class Queue {
 
 	void enQueue(int x)
 	{
-		// Move all elements from s1 to s2
 		while (!s1.empty()) {
 			s2.push(s1.top());
 			s1.pop();
 		}
-
-		// Push item into s1
 		s1.push(x);
-
-		// Push everything back to s1
 		while (!s2.empty()) {
 			s1.push(s2.top());
 			s2.pop();
 		}
 	}
-
-	// Dequeue an item from the queue
 	int deQueue()
 	{
-		// if first stack is empty
 		if (s1.empty()) {
 			cout << "Q is Empty";
 		}
-
-		// Return top of s1
 		int x = s1.top();
 		s1.pop();
 		return x;
@@ -44,8 +34,6 @@ class Queue {
       if (s1.empty()) {
 			cout << "Q is Empty";
 		}
-
-		// Return top of s1
 		int x = s1.top();
 		return x;
     }
@@ -72,25 +60,16 @@ int main()
 class Queue {
     public:
 	stack<int> s1, s2;
-
-	// Enqueue an item to the queue
 	void enQueue(int x)
 	{
-		// Push item into the first stack
 		s1.push(x);
 	}
-
-	// Dequeue an item from the queue
 	int deQueue()
 	{
-		// if both stacks are empty
 		if (s1.empty() && s2.empty()) {
 			cout << "Q is empty";
 			exit(0);
 		}
-
-		// if s2 is empty, move
-		// elements from s1
 		if (s2.empty()) {
 			while (!s1.empty()) {
 				s2.push(s1.top());
@@ -98,28 +77,21 @@ class Queue {
 			}
 		}
 
-		// return the top item from s2
 		int x = s2.top();
 		s2.pop();
 		return x;
 	}
     int peek(){
-        // if both stacks are empty
 		if (s1.empty() && s2.empty()) {
 			cout << "Q is empty";
             exit(0);
 		}
-
-		// if s2 is empty, move
-		// elements from s1
 		if (s2.empty()) {
 			while (!s1.empty()) {
 				s2.push(s1.top());
 				s1.pop();
 			}
 		}
-
-		// return the top item from s2
 		int x = s2.top();
 		return x;
 	
